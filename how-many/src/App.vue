c837cca1e27d65f2774058afb7197d0c31c38779
向山　翔 <hms0175km@gmail.com>
2021-12-14 19:22:03 +0900

    [Fix]footer

--- how-many/src/App.vue
+++ how-many/src/App.vue
@@ -75,7 +75,7 @@ export default {
   
   .main {
     width: 70%;
-    margin: 15px auto 15px auto;
+    padding: 15px auto 15px auto;
   }
   footer {
     background-color:cadetblue;
@@ -86,7 +86,7 @@ export default {
   @media screen and (max-width: 900px) {
     .main {
       width: 100%;
-      margin: 15px auto 0px auto;
+      padding: 15px auto 0px auto;
     }
     footer {
       margin-bottom: 0;
